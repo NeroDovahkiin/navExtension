139a3026adb3531f5426294b09e938a5273c0a69
NeroDovahkiin <federixo_fgm@hotmail.com>
2023-04-12 02:26:47 -0300

    nuevo

--- script.js
+++ script.js
@@ -16,9 +16,43 @@ slider.addEventListener('input', e => {
     label.innerText = e.target.value + "px";
 });
 
-select.addEventListener('change',  e => {
+select.addEventListener('change', e => {
     let selectedIndex = select.selectedOptions[0].value;
     document.documentElement.style.setProperty('--numColumns', selectedIndex);
+    //
+    document.documentElement.style.setProperty('--cardWidth', '200px');
+    label.innerText = "200px";
+    
+
+
+    //new
+    switch (selectedIndex) {
+        case '4':
+            document.getElementById("rangeCards").min = 200;
+            document.getElementById("rangeCards").max = 440;
+            break;
+        case '5':
+            document.getElementById("rangeCards").min = 200;
+            document.getElementById("rangeCards").max = 346;
+            break;
+        case '6':
+            document.getElementById("rangeCards").min = 200;
+            document.getElementById("rangeCards").max = 287;
+            break;
+        case '7':
+            document.getElementById("rangeCards").min = 200;
+            document.getElementById("rangeCards").max = 244;
+            break;
+        case '8':
+            document.getElementById("rangeCards").min = 150;
+            document.getElementById("rangeCards").max = 200;
+            break;
+        default:
+            document.getElementById("rangeCards").min = 200;
+            document.getElementById("rangeCards").max = 580;
+    }
+    document.getElementById("rangeCards").value = 200;
+    
 });
 
 console.info("card width current value: " + getComputedStyle(document.documentElement).getPropertyValue('--cardWidth'));
